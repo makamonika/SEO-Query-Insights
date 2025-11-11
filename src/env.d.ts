bb15b83c298b82622394d6a5e0e729f8624a1496
makamonika <maka.monika0@gmail.com>
2025-11-11 18:33:51 +0100

    chore: update pr workflow

--- src/env.d.ts
+++ src/env.d.ts
@@ -19,11 +19,6 @@ declare global {
 interface ImportMetaEnv {
   readonly SUPABASE_URL: string;
   readonly SUPABASE_KEY: string;
-  readonly OPENROUTER_API_KEY: string;
-  readonly IMPORT_SOURCE_BASE_URL: string;
-  readonly IMPORT_FETCH_TIMEOUT_MS?: string;
-  readonly IMPORT_MAX_BYTES?: string;
-  // more env variables...
 }
 
 interface ImportMeta {
